45c09239070b0636720681d7645bc12e85e196c8
TUCCHI1 <tutikikin1@gmail.com>
2025-02-27 16:11:22 +0900

    feat(next): enable standalone output in Next.js configuration
    Added support for standalone output in the Next.js configuration to enhance deployment flexibility.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	
-	// トランスパイルするパッケージ（UIコンポーネントはここに含める）
-	transpilePackages: ["ui", "utils", "antd"],
-	
-	// 実験的な機能
-	experimental: {
-	  // サーバーアクションの設定
-	  serverActions: {
-		allowedOrigins: ['localhost:3000', '192.168.0.3:3000'],
-		bodySizeLimit: '2mb'
-	  }
-	},
-  };
-	
+    reactStrictMode: true,
+    
+    // トランスパイルするパッケージ（UIコンポーネントはここに含める）
+    transpilePackages: ["ui", "utils", "antd"],
+    
+    // スタンドアロン出力を有効化
+    output: 'standalone',
+    
+    // 実験的な機能
+    experimental: {
+      // サーバーアクションの設定
+      serverActions: {
+        allowedOrigins: ['localhost:3000', '192.168.0.3:3000'],
+        bodySizeLimit: '2mb'
+      }
+    },
+};
+    
 module.exports = nextConfig;
